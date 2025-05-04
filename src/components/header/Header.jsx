f2d6b130bd68ff7d9613fa08bc9d4c9a600edcd6
osmanbybd <mreosmangoni@gmail.com>
2025-05-05 01:36:45 +0600

    second comit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center my-6'>
             <img src={logo} alt="" />
-            <p className='text-accent mt-3'>Journalism Without Fear or Favour</p>
+            <p className='text-accent mt-3'>The Bangladesh news </p>
             <p className='font-semibold text-accent'>{format(new Date (), "EEEE, MMMM ,dd , yyyy")}</p>
             <p className='font-semibold text-accent'>{format(new Date (), "hh.mm. ss a")}</p>
         </div>
